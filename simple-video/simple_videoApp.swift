6c4b9f70060bce89124714074b0a8b23083b6e14
Daniel Perry <dperry@danieltperry.me>
2024-06-30 10:29:30 -0400

    Initial Commit

---
+++ simple-video/simple_videoApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_videoApp.swift
+//  simple-video
+//
+//  Created by Daniel Perry on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct simple_videoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
